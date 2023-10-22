a255be48668cbc2c025e2b82aa1ba21d1e5f8c40
112ab0029 <146065252+112ab0029@users.noreply.github.com>
2023-10-22 23:21:11 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+typedef struct {
+    int start;
+    int end;
+} Order;
+
+int compare(const void *a, const void *b) {
+    return ((Order *)a)->start - ((Order *)b)->start;
+}
+
+int main() {
+    Order orders[3];
+    int numVehicles = 0;
+
+    for (int i = 0; i < 3; i++) {
+        printf("輸入第 %d 筆訂單的出發時間和返回時間（範例：8 16）：", i + 1);
+        scanf("%d %d", &orders[i].start, &orders[i].end);
+    }
+
+    qsort(orders, 3, sizeof(Order), compare);
+
+    for (int i = 0; i < 3; i++) {
+        if (orders[i].start >= numVehicles) {
+            numVehicles++;
+        }
+    }
+
+    printf("至少需要 %d 輛遊覽車才足以應付需求。\n", numVehicles);
+
+    return 0;
+}
