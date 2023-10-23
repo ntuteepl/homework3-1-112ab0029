2ba93247827f3e2f0eccd868e53e9e547c8863b9
112ab0029 <146065252+112ab0029@users.noreply.github.com>
2023-10-23 23:37:25 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-int digitSum(int n) {
+
+int digitSum(int num) {
     int sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n /= 10;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
     }
     return sum;
 }
 
-int compare(const void *a, const void *b) {
-    int sum_a = digitSum(*(int *)a);
-    int sum_b = digitSum(*(int *)b);
-    if (sum_a != sum_b) {
-        return sum_a - sum_b;
-    } else {
-        return *(int *)a - *(int *)b;
-    }
-}
-
 int main() {
-    int N;
-    printf("請輸入整數的個數 N：");
-    scanf("%d", &N);
+    int num1, num2;
+    printf("請輸入兩個整數，用空格隔開：\n");
+    scanf("%d %d", &num1, &num2);
 
-    int numbers[N];
+    int sum1 = digitSum(num1);
+    int sum2 = digitSum(num2);
 
-    for (int i = 0; i < N; i++) {
-        printf("請輸入第 %d 個整數：", i + 1);
-        scanf("%d", &numbers[i]);
+    if (sum1 < sum2 || (sum1 == sum2 && num1 < num2)) {
+        printf("計算後的最小整數是：%d\n", num1);
+    } else {
+        printf("計算後的最小整數是：%d\n", num2);
     }
-
-    qsort(numbers, N, sizeof(int), compare);
-
-    printf("排序後的結果：\n");
-    for (int i = 0; i < N; i++) {
-        printf("%d ", numbers[i]);
-    }
-    printf("\n");
-
-    printf("最小數值：%d\n", numbers[0]);
 
     return 0;
 }
